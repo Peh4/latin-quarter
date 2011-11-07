51056a0adf8e6602fc896270919e270cec585281
peh4 <peha@free.fr>
2011-11-07 16:56:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LatinQuarterApp::Application.config.secret_token = '9b6a32ef14244051ce875ea9c28f7d0314024c95862c7e1ee143c7950437483f43d3481696d52eec700a47e745ba00ea4c23be1bc6831fcc05daf91b29636d20'
